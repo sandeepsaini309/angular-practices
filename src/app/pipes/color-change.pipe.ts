4ab7d3fe07ee24b1c4397a94b6f36ef8463a1e90
Sandeep309 <sandeepsaini6237@gmail.com>
2024-06-18 12:23:27 +0530

    text color change using pipe

--- src/app/pipes/color-change.pipe.ts
+++ src/app/pipes/color-change.pipe.ts
@@ -1,10 +1,25 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Pipe({
   name: 'colorChange',
 })
 export class ColorChangePipe implements PipeTransform {
-  transform(value: string, ...args: unknown[]): unknown {
-    return value.replaceAll('india', `<span style="color:blue;">india</span>`);
+  constructor(private sanitizer: DomSanitizer) {}
+  transform(value: string, search: string): unknown {
+    // const replacedValue = value.replaceAll(
+    //   search,
+    //   `<span style="color:blue;">${search}</span>`
+    // );
+    // return replacedValue;
+
+    // Regular expression to find all instances of the search term
+    const re = new RegExp(search, 'gi');
+    const replacedValue = value.replace(
+      re,
+      `<span style="color:blue;">${search}</span>`
+    );
+    // Sanitize the HTML before returning it
+    return this.sanitizer.bypassSecurityTrustHtml(replacedValue);
   }
 }
