15f60d1adf2f7bbde7ebe1afa8d4cc6609f7a536
Sandeep309 <sandeepsaini6237@gmail.com>
2024-07-05 23:38:23 +0530

    viewchild single instance

--- src/app/components/html-css/html-css.component.ts
+++ src/app/components/html-css/html-css.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { TaskComponent } from '../task/task.component';
 
 @Component({
   selector: 'app-html-css',
   templateUrl: './html-css.component.html',
-  styleUrls: ['./html-css.component.scss']
+  styleUrls: ['./html-css.component.scss'],
 })
 export class HtmlCssComponent {
+  @ViewChild('task') taskM!: TaskComponent;
 
+  ngAfterViewInit() {
+    console.log('this.taskM.list1', this.taskM.list1);
+  }
 }
